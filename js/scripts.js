aca4175dd65fd5b28a1b1b141d454c8b66b4fad1
Nhat Hoang and Ryan Apking <ryan@>
2016-08-15 16:46:18 -0700

    create a deck of cards using an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,64 @@
+var deck = [];
+
 $(document).ready(function(){
+  setDeck();
+
+  $("#resetCards").click(function() {
+    setDeck();
+  });
+
+  $("#numberSortBtn").click(function() {
+    deck.sort();
+    display(deck);
+  });
+
+  $("#heartsBtn").click(function() {
+    deck = deck.filter(function(card){
+      return checkSuit(card, "hearts");
+    });
+    display(deck);
+  });
+
+  $("#clubsBtn").click(function() {
+    deck = deck.filter(function(card){
+      return checkSuit(card, "clubs");
+    });
+    display(deck);
+  });
+  $("#spadesBtn").click(function() {
+    deck = deck.filter(function(card){
+      return checkSuit(card, "spades");
+    });
+    display(deck);
+  });
+  $("#diamondsBtn").click(function() {
+    deck = deck.filter(function(card){
+      return checkSuit(card, "diamonds");
+    });
+    display(deck);
+  });
+});
+
+function checkSuit(card, suit) {
+  return card.includes(suit);
+};
+
+function setDeck() {
+  deck = [];
   var decknumber = ["ace",2,3,4,5,6,7,8,9,10,"jack","queen","king"];
-  var deck = [];
-  var suits = ["clubs", "spades", "diamonds", "hearts"];
-
+  var suits = ["clubs","diamonds", "hearts", "spades"];
   suits.forEach(function(suit){
-
     var tempDeck = decknumber.map(function(card) {
       return card + " of " + suit;
     });
-
     deck = deck.concat(tempDeck);
   });
+  display(deck);
+};
 
-// this code has been simplified above
-  // var suitClub = decknumber.map(function(card) {
-  //   return card + " of clubs";
-  // });
-  // var suitSpade = decknumber.map(function(card) {
-  //   return card + " of spades";
-  // });
-  // var suitDiamond = decknumber.map(function(card) {
-  //   return card + " of diamonds";
-  // });
-  // var suitHeart = decknumber.map(function(card) {
-  //   return card + " of hearts";
-  // });
-  // deck = deck.concat(suitClub).concat(suitSpade).concat(suitHeart).concat(suitDiamond);
-
-  deck.forEach(function(card) {
+function display(displayDeck) {
+  $("ul").text("");
+  displayDeck.forEach(function(card) {
     $("ul").append("<li>" + card + "</li>");
   });
-});
+};
